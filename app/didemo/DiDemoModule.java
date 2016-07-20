d6174aafda3e782b52c494f277db371ddbd578c3
Panshul Gupta <panshul.gupta@commercetools.de>
2016-07-20 11:33:33 +0200

    singleton and request scope example.

--- app/didemo/DiDemoModule.java
+++ app/didemo/DiDemoModule.java
@@ -3,17 +3,21 @@ package didemo;
 import com.commercetools.sunrise.common.contexts.RequestScoped;
 import com.google.inject.AbstractModule;
 
-import javax.inject.Singleton;
-
 public class DiDemoModule extends AbstractModule {
     @Override
     protected void configure() {
         // CLASS
 //        bind(InjectionSubject.class).in(Singleton.class);
-//        bind(InjectionSubject.class).in(RequestScoped.class);
+        bind(InjectionSubject.class).in(RequestScoped.class);
 
         // SUBCLASS
 //        bind(SubclassInjectionSubject.class).in(Singleton.class);
-//        bind(SubclassInjectionSubject.class).in(RequestScoped.class);
+        bind(SubclassInjectionSubject.class).in(RequestScoped.class);
     }
 }
+
+/**
+ * Request Scope - instantiates the class on every request.
+ * Singleton - only once
+ * default scope -> prototype scope
+ */
